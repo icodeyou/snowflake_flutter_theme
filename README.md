aa340cf4e31125f39c555f9b583aa507873f6b52
Jean Vallon <jvallon44@gmail.com>
2023-04-02 02:41:04 +0200

    refactor: recreate project with flutter create --template [Remove  everything]

--- README.md
+++
@@ -1,4 +0,0 @@
-# snowflake_flutter_theme
-
-This is a theme that can be imported in a Flutter project.
-It gives the constants and the widgets that can be used to design your ui.
--- lib/snowflake_flutter_theme.dart
+++
@@ -1,8 +0,0 @@
-library snowflake_flutter_theme;
-
-export 'src/style/snowflake_theme_data.dart';
-export 'src/widgets/buttons.dart';
-export 'src/widgets/error_details.dart';
-export 'src/widgets/gap.dart';
-export 'src/widgets/loader.dart';
-export 'src/widgets/text.dart';
