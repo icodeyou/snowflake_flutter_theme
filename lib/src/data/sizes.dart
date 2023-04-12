5bd5e982b9edc3f18a53f19dab7bc8cdd25f53f6
Jean Vallon <jvallon44@gmail.com>
2023-04-12 19:34:51 -0400

    feat: add extensions horizontalOnly and verticalOnly on EdgeInsets

--- lib/src/data/sizes.dart
+++ lib/src/data/sizes.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 /// All sizes (height and width, paddings, margins) are defined in this class
 
 class ThemeSizes {
-  
   /// XXXS size
   static const double xxxs = 2;
 
@@ -34,7 +33,15 @@ class ThemeSizes {
 
 /// Size extensions on double
 extension ThemeSizesExtensions on double {
-
   /// Transform double into EdgeInsets, applied on all sides
   EdgeInsets get asInsets => EdgeInsets.all(this);
+}
+
+/// Size extensions on EdgeInsets
+extension EwaSizesInsetsExtensions on EdgeInsets {
+  /// Remove vertical borders
+  EdgeInsets get horizontalOnly => copyWith(top: 0, bottom: 0);
+
+  /// Remove horizontal borders
+  EdgeInsets get verticalOnly => copyWith(right: 0, left: 0);
 }
