df5168a92451ef170ec929aff8f79b5cf817d215
Jean Vallon <jvallon44@gmail.com>
2023-07-15 22:16:09 +0200

    refactor: change decorations (rename lightBox and add greyBox)

--- lib/src/data/decorations.dart
+++ lib/src/data/decorations.dart
@@ -5,7 +5,7 @@ import 'package:snowflake_flutter_theme/src/data/radius.dart';
 /// All decorations are defined in this class
 class ThemeDecorations {
   /// Create an elevated box with a light background, rounded borders
-  static final BoxDecoration lightBox = BoxDecoration(
+  static final BoxDecoration lightCard = BoxDecoration(
     color: Colors.white,
     borderRadius: ThemeRadius.m.asBorderRadius,
     boxShadow: <BoxShadow>[
@@ -14,5 +14,10 @@ class ThemeDecorations {
         offset: const Offset(0, 3), // changes position of shadow
       ),
     ],
+  );
+
+  static final BoxDecoration greyBox = BoxDecoration(
+    color: Colors.white,
+    borderRadius: ThemeRadius.m.asBorderRadius,
   );
 }
