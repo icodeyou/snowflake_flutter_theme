7c0505dc5dc897ee62809780005c81236349c3bc
Jean Vallon <jvallon44@gmail.com>
2024-05-27 12:32:30 +0200

    fix: update version script

--- update_version.sh
+++ update_version.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Example : Last tag is 0.0.6
+# Old version : 0.0.6
+# New version : 0.0.7
+# Next version : 0.0.8
+
 echo "🚀 Let's update the Snowflake version !"
 
 # Function to check for uncommitted changes
@@ -22,27 +27,28 @@ check_branch_exists() {
 # Function to get the latest tag
 get_latest_tag() {
     git fetch --tags || exit
-    latest_tag=$(git describe --tags `git rev-list --tags --max-count=1`)
-    if [ -z "$latest_tag" ]; then
+    old_version=$(git describe --tags `git rev-list --tags --max-count=1`)
+    if [ -z "$old_version" ]; then
         echo "Error: No tags found in the repository."
         exit 1
     fi
-    echo $latest_tag
+    echo $old_version
 }
 
 # Check for uncommitted changes
 check_uncommitted_changes
 
 # Get the latest tag
-latest_tag=$(get_latest_tag)
-echo "The latest version is $latest_tag"
+old_version=$(get_latest_tag)
+echo "The latest version is $old_version"
 
 # Prompt for the new version and old build number
 read -p "Enter the new version : " new_version
-read -p "Enter the new build number : " new_build_number
+read -p "Enter the next version : " next_version
+read -p "Enter the new build number : " next_build_number
 
 # Increment the build number
-next_build_number=$((new_build_number + 1))
+new_build_number=$((next_build_number - 1))
 
 # Check if the old build branch exists
 check_branch_exists "v${new_build_number}"
@@ -52,16 +58,16 @@ git merge "v${new_build_number}" -m 'new status colors' --no-ff || exit
 
 # Fetch the latest changes from the remote repository and display the log
 git pull || exit
-git log --oneline --decorate --graph --all || exit
+git --no-pager log --date=format:"%Y-%m-%d %H:%M:%S" --pretty=format:"%C(yellow)%h%Cgreen -- %ad %C(magenta)[%an]%Creset : %s" -10 || exit
 
 # Tag the commit with the tag ${old_version} and the message 'new status colors'
-git tag "${old_version}" -m 'new status colors' || exit
+git tag "${new_version}" -m 'new status colors' || exit
 
 # Push the tags to the remote repository
 git push --tags || exit
 
 # Update the version and build number in pubspec.yaml
-sed -i '' "s/version: ${old_version}+[0-9]*/version: ${new_version}+${next_build_number}/" pubspec.yaml || exit
+sed -i '' "s/version: ${new_version}+[0-9]*/version: ${next_version}+${next_build_number}/" pubspec.yaml || exit
 
 # Commit the changes with the message '[DELIVERY ${old_version}]: upgrade version to ${new_version}'
 git commit -am "[DELIVERY ${old_version}]: upgrade version to ${new_version}" || exit
