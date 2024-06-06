b7422a7bcbc51adc9dc08a3a714eb60aa21659fc
Jean Vallon <jvallon44@gmail.com>
2024-06-06 18:52:06 +0200

    feat: remove native toast method now that BotToast handles it

--- lib/src/helpers/extensions/context_extensions.dart
+++ lib/src/helpers/extensions/context_extensions.dart
@@ -1,17 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:snowflake_flutter_theme/src/helpers/notif.dart';
 
 /// This class defines extensions on build contexts
 extension ContextExtensions on BuildContext {
-  /// Show a toast message
-  void showToast(
-    String message,{
-    ToastType? type,
-    Duration? duration,
-  }) {
-    Notif.showToast(message, context: this, type: type, duration: duration);
-  }
-
   /// Return true if the app is in dark mode
   bool get isDarkMode => Theme.of(this).brightness == Brightness.dark;
 }
