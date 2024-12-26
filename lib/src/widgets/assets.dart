a8fa76721a4447bf27a6c44f9db25a9524c78d92
Jean Vallon <jvallon44@gmail.com>
2024-12-26 17:35:00 +0100

    feat: add size parameter to AppAsset

--- lib/src/widgets/assets.dart
+++ lib/src/widgets/assets.dart
@@ -7,18 +7,27 @@ class AppAsset extends StatelessWidget {
     required this.iconName,
     this.width,
     this.height,
-  });
+    this.size,
+  })  : assert(
+          !(width != null && size != null),
+          'size and width cannot be provided together',
+        ),
+        assert(
+          !(height != null && size != null),
+          'size and height cannot be provided together',
+        );
 
   final String iconName;
   final double? width;
   final double? height;
+  final double? size;
 
   @override
   Widget build(BuildContext context) {
     return Image.asset(
       'assets/$iconName',
-      width: width,
-      height: width,
+      width: size ?? width,
+      height: size ?? height,
     );
   }
 }
@@ -27,22 +36,31 @@ class AppAssetSvg extends StatelessWidget {
   const AppAssetSvg({
     super.key,
     required this.iconName,
+    this.color,
     this.width,
     this.height,
-    this.color,
-  });
+    this.size,
+  })  : assert(
+          !(width != null && size != null),
+          'size and width cannot be provided together',
+        ),
+        assert(
+          !(height != null && size != null),
+          'size and height cannot be provided together',
+        );
 
   final String iconName;
+  final Color? color;
   final double? width;
   final double? height;
-  final Color? color;
+  final double? size;
 
   @override
   Widget build(BuildContext context) {
     return SvgPicture.asset(
       'assets/$iconName',
-      width: width,
-      height: height,
+      width: size ?? width,
+      height: size ?? height,
       colorFilter:
           color == null ? null : ColorFilter.mode(color!, BlendMode.srcIn),
     );
