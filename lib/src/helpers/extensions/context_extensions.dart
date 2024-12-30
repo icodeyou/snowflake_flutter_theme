30132d99b3c54af818c64e453f2dbdba9c3d8b5e
Jean Vallon <jvallon44@gmail.com>
2024-12-30 09:43:14 +0100

    fix: context extension

--- lib/src/helpers/extensions/context_extensions.dart
+++ lib/src/helpers/extensions/context_extensions.dart
@@ -4,12 +4,19 @@ import 'package:snowflake_flutter_theme/src/helpers/notif.dart';
 /// This class defines extensions on build contexts
 extension ContextExtensions on BuildContext {
   /// Show a toast message
-  void showToast(
-    String message,{
+  void showToast({
+    required String title,
+    required String message,
     ToastType? type,
     Duration? duration,
   }) {
-    Notif.showToast(message, context: this, type: type, duration: duration);
+    Notif.showToast(
+      title: title,
+      message: message,
+      context: this,
+      type: type,
+      duration: duration,
+    );
   }
 
   /// Return true if the app is in dark mode
