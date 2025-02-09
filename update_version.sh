e5ce5f799702b56a77f5566964cc623977949a2c
Jean Vallon <jvallon44@gmail.com>
2025-02-09 19:12:37 +0100

    new update script taking breaking changes into account

--- update_version.sh
+++ update_version.sh
@@ -4,24 +4,31 @@ echo ""
 echo "🚀 Let's update the Snowflake version!"
 echo ""
 
+#logMethod
+🔶() {
+    echo -e "\n🔶 $1\n"
+}
+
 # Helper function to display an error message and exit
 error_exit() {
     echo "❌ Error: $1"
     exit 1
 }
 
-# Function to check for uncommitted changes
-check_uncommitted_changes() {
+# Function to check if there are uncommitted changes
+has_uncommitted_changes() {
     if ! git diff-index --quiet HEAD --; then
-        error_exit "You have uncommitted changes. Please commit or stash them before running this script."
+        return 0 # Uncommitted changes exist
+    else
+        return 1 # No uncommitted changes
     fi
 }
 
 # Function to check if a branch exists
 check_branch_exists() {
     local branch=$1
-    if ! git show-ref --verify --quiet refs/heads/"$branch"; then
-        error_exit "Branch '$branch' does not exist."
+    if git show-ref --verify --quiet refs/heads/"$branch"; then
+        error_exit "Branch '$branch' already exists. Choose another version number."
     fi
 }
 
@@ -35,10 +42,16 @@ get_latest_tag() {
     echo "$old_version"
 }
 
-echo -e "\n🔶 Check for uncommited changes\n"
-check_uncommitted_changes
+# If there are uncommitted changes, prompt the user for a commit message
+if has_uncommitted_changes; then
+    echo ""
+    echo "⚠️  You have uncommitted changes."
+    echo "If they are BREAKING CHANGES, we will commit them to the new branch, otherwise cancel and commit your changes."
+    echo "Enter a commit message for the breaking changes: "
+    read -r user_commit_message
+fi
 
-echo -e "\n🔶 Get latest tag\n"
+🔶 'Get latest tag'
 old_version=$(get_latest_tag)
 echo "ℹ️  The latest version is $old_version"
 echo ""
@@ -47,52 +60,55 @@ echo ""
 read -p "Enter the new version (we override current pubspec): " new_version
 read -p "Enter the next version (temporary in pubspec): " next_version
 read -p "Enter the next build number (next branch number): " next_build_number
-read -p "Enter the commit message for the merge and tag: " commit_message
 
-echo -e "\n🔶 Increment build number\n"
-new_build_number=$((next_build_number - 1))
+echo -e "\n🔶 Check that new branch v${next_build_number} doesn't already exist"
+check_branch_exists "v${next_build_number}"
 
-echo -e "\n🔶 Check old build branch exists"
-check_branch_exists "v${new_build_number}"
+🔶 'Create and switch to the new branch v${next_build_number}'
+git checkout -b "v${next_build_number}" || error_exit "Failed to create and switch to the branch 'v${next_build_number}'."
 
-# [CHECK] SED NEW
-# Before push, update the NEW version and build number in pubspec.yaml, if it wasn't already done
-echo -e "\n🔶 Replace version in pubspec.yaml"
-sed -i '' "s/^version:.*/version: ${new_version}+${new_build_number}/" pubspec.yaml
+🔶 'Commit last changed to new branch'
+if has_uncommitted_changes; then
+    git add .
+    git commit -m "$user_commit_message" || error_exit "Failed to commit uncommitted changes."
+fi
 
-echo -e "\n🔶 Commit if necessary"
-git commit -am "pub: update version to ${new_version}+${new_build_number}"
+🔶 'Replace version in pubspec.yaml'
+sed -i '' "s/^version:.*/version: ${new_version}+${next_build_number}/" pubspec.yaml
 
-echo -e "\n🔶 Checkout master"
-git checkout master
+🔶 'Commit version update'
+git commit -am "pub: update version to ${new_version}+${next_build_number}" || error_exit "Failed to commit version update."
 
-echo -e "\n🔶 Merge branch v${new_build_number} with the provided commit message"
-git merge "v${new_build_number}" -m "$commit_message" --no-ff || error_exit "Failed to merge branch 'v${new_build_number}'."
+🔶 'Checkout master'
+git checkout master || error_exit "Failed to switch to master branch."
 
-echo -e "\n🔶 Fetch the latest changes from the remote repository and display the log\n" 
+🔶 'Merge v${next_build_number} into master'
+git merge "v${next_build_number}" -m "Merge v${next_build_number} into master" --no-ff || error_exit "Failed to merge branch 'v${next_build_number}'."
+
+🔶 'Fetch latest changes and display log\n'
 git pull || error_exit "Failed to pull the latest changes from the remote repository."
 git --no-pager log --date=format:"%Y-%m-%d %H:%M:%S" --pretty=format:"%C(yellow)%h%Cgreen -- %ad %C(magenta)[%an]%Creset : %s" -10 || error_exit "Failed to display the git log."
 
-echo -e "\n🔶 Tag the commit with the tag ${new_version} and the provided commit message"
-git tag "${new_version}" -m "$commit_message" || error_exit "Failed to tag the commit with version ${new_version}."
+🔶 'Tag the commit with ${new_version}'
+git tag "${new_version}" -m "Release ${new_version}" || error_exit "Failed to tag the commit with version ${new_version}."
 
-echo -e "\n🔶 Push the tags to the remote repository"
+🔶 'Push the tags to the remote repository'
 git push --tags || error_exit "Failed to push the tags to the remote repository."
 
-# [CHECK] SED NEXT
-echo -e "\n🔶 Update the NEXT version and build number in pubspec.yaml"
+🔶 'Update the NEXT version and build number in pubspec.yaml'
 sed -i '' "s/^version:.*/version: ${next_version}+${next_build_number}/" pubspec.yaml
-echo -e "\n🔶 Commit the changes with the message 'DELIVERY ...'"
+
+🔶 'Commit the changes for next version'
 git commit -am "[DELIVERY ${old_version}]: upgrade version to ${new_version}" || error_exit "Failed to commit the version update."
 
-echo -e "\n🔶 Push the changes to master"
+🔶 'Push changes to master'
 git push || error_exit "Failed to push the changes to the master branch."
 
-echo -e "\n🔶 Create and switch to a new branch v${next_build_number}"
-git checkout -b "v${next_build_number}" || error_exit "Failed to create and switch to the branch 'v${next_build_number}'."
+🔶 'Switch back to v${next_build_number}'
+git checkout "v${next_build_number}" || error_exit "Failed to switch back to 'v${next_build_number}'."
 
-echo -e "\n🔶 Push new branch"
-git push || error_exit "Failed to push the branch 'v${next_build_number}'."
+🔶 'Push new branch'
+git push --set-upstream origin "v${next_build_number}" || error_exit "Failed to push the branch 'v${next_build_number}'."
 
 # Print a message indicating completion
 echo ""
