238b7115aab7d542d6b392048417f79835236a27
Jean Vallon <jvallon44@gmail.com>
2025-04-17 13:04:26 +0200

    feat: onBackground color by default for error messages

--- lib/src/widgets/error_details.dart
+++ lib/src/widgets/error_details.dart
@@ -10,13 +10,15 @@ class AppErrorDetailsWidget extends StatelessWidget {
   const AppErrorDetailsWidget({
     required this.errorTitle,
     required this.errorMessage,
+    this.red = false,
     this.retryButton,
     Key? key,
   }) : super(key: key);
 
   final String errorTitle;
   final String errorMessage;
-  final ({String label, VoidCallback callback})? retryButton; 
+  final bool red;
+  final ({String label, VoidCallback callback})? retryButton;
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +27,17 @@ class AppErrorDetailsWidget extends StatelessWidget {
         padding: ThemeSizes.m.asInsets,
         child: Column(
           children: [
-            AppText.l(errorTitle, color: ThemeColors.statusError),
+            AppText.l(
+              errorTitle,
+              color: red
+                  ? ThemeColors.statusError
+                  : ThemeColors.onBackground(context),
+            ),
             AppText.m(
               errorMessage,
-              color: ThemeColors.statusError,
+              color: red
+                  ? ThemeColors.statusError
+                  : ThemeColors.onBackground(context),
             ),
             const AppGap.m(),
             if (retryButton != null)
